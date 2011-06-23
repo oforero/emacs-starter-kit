82131399e77bd471632e54c7e9d783c9694e99e8
Oscar Forero <omforero@googlemail.com>
2011-06-23 15:50:30 +0200

    Add and wire the starter-kit-scala
    Signed-off-by: Oscar Forero <omforero@googlemail.com>

---
+++ starter-kit-scala.el
@@ -0,0 +1,42 @@
+;; Scala configuration
+
+;; Yasnippet
+(add-to-list 'load-path (concat dotfiles-dir "/plugins")
+(require 'yasnippet-bundle)
+
+;; Yasnippet
+;; (add-to-list 'load-path (concat dotfiles-dir "yasnippet"))
+;; (require 'yasnippet)
+;; (yas/initialize)
+
+;; (setq yas/scala-directory (concat dotfiles-dir  "yasnippet/snippets/text-mode/scala-mode/"))
+;; (yas/load-directory yas/scala-directory)
+
+;; Scala-mode
+(add-to-list 'load-path (concat dotfiles-dir "scala-mode"))
+(require 'scala-mode-auto)
+
+(add-hook 'scala-mode-hook
+          '(lambda ()
+             (yas/minor-mode-on)))
+
+;; Load the ensime lisp code...
+(add-to-list 'load-path (concat dotfiles-dir "ensime/elisp"))
+(require 'ensime)
+
+;; This step causes the ensime-mode to be started whenever
+;; scala-mode is started for a buffer. You may have to customize this step
+;; if you're not using the standard scala mode.
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+
+;; Show numbers in scala mode
+(add-hook 'scala-mode-hook
+  (lambda ()
+    (linum-mode))) 
+
+;; Provides scala starter kit
+(provide 'starter-kit-scala)
+
+;; MINI HOWTO:
+;; Open .scala file. M-x ensime (once per project)
+
